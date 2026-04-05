9b0439bc74e7873497aeb104aee46734877b37cb
Abdul Kareem Khan <infostndmaketech@gmail.com>
2026-04-05 15:21:35 +0530

    fix: update APK URL in version controller and remove version.json

--- server/controllers/version.controller.js
+++ server/controllers/version.controller.js
@@ -1,7 +1,7 @@
 export const version = async (_, res) => {
     res.json({
         latestVersion: "1.0.0",
-        apkUrl: "https://github.com/techstandakmr/ai-chat-assistant-app-deployed/releases/download/v1.0.0/ai-chat-assistant.apk",
+        apkUrl: "https://github.com/techstandakmr/ai-chat-assistant-app/releases/download/v1.0.0/ai-chat-assistant.apk",
         forceUpdate: true
     });
 }
